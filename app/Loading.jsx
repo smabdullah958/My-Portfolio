20bed906ac4b50fdcc4c9979a0e9cb625313d25f
abdullah <smabdulla958>
2025-10-21 02:58:49 +0500

    add loader

--- app/Loading.jsx
+++ app/Loading.jsx
@@ -1,13 +1,9 @@
-
 import React from "react";
-
 const Loading = () => {
   return (
       <div className="flex items-center justify-center min-h-screen bg-gray-900">
       <div className="w-16 h-16 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
     </div>
-
   );
 };
-
 export default Loading;
