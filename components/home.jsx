db0e47efc895db152d97426bb4af6f7d499a5ead
abdullah <smabdulla958>
2025-11-04 02:01:14 +0500

    change a cv brother

--- components/home.jsx
+++ components/home.jsx
@@ -88,7 +88,7 @@ const HomePage = () => {
         </div>
       </div>
            
-        <Link href="/S_M_Abdullah.pdf" target="_blank" className=" fixed text-end bottom-4 right-5  z-50 px-4 py-3 bg-[#4e8bca] text-white font-semibold text-xl rounded-2xl  hover:bg-blue-600 transition-all duration-300">CV</Link>         
+        <Link href="/abdullah_CV.pdf" target="_blank" className=" fixed text-end bottom-4 right-5  z-50 px-4 py-3 bg-[#4e8bca] text-white font-semibold text-xl rounded-2xl  hover:bg-blue-600 transition-all duration-300">CV</Link>         
          
 
 
