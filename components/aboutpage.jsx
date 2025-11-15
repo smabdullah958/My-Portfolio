380db5b77f3dda9fc072a89b223a88d5a4ca6b5e
abdullah <smabdulla958>
2025-11-16 00:48:08 +0500

    change the position of a image

--- components/aboutpage.jsx
+++ components/aboutpage.jsx
@@ -10,7 +10,7 @@ const aboutpage = () => {
        data-aos-duration="500">
      <h1 className='text-3xl font-bold pt-5 sm:pt-10 pb-5 text-center'>About Me</h1>
        <div className='flex flex-col md:grid md:grid-cols-3 gap-12'>
-        <div className="text-justify md:col-span-2 md:text-[16px] flex-col justify-center items-center content-center ">
+        <div className="text-justify md:col-span-2 md:text-[16px] flex-col justify-center items-center content-center order-2 md:order-1">
     I am a passionate <span className='text-blue-400 font-bold'>Full Stack Developer </span> specializing in the <span className='text-blue-400 font-bold'>MERN stack</span> (MongoDB, Express.js, React, Node.js).
 I create scalable, modern, and user-friendly web applications with clean, maintainable code.
 With expertise in both frontend and backend, I focus on seamless UI/UX and secure backend systems.
@@ -18,7 +18,7 @@ I enjoy problem-solving, exploring new technologies, and building impactful digi
 Open to both remote and on-site opportunities, my goal is to grow as a developer while helping businesses 
 achieve their vision.
 </div>
-<div className="flex justify-center pt-5">
+<div className="flex justify-center pt-5 order-1 md:order-2">
           <Image
             src="/me (2).jpeg"
             alt="My Image"
