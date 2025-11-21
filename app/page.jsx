1dc10c0c49449a964b804335ffed227202a05f2b
abdullah <smabdulla958>
2025-11-22 01:57:55 +0500

    add some delay to show a loader

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,9 @@ import ContactUs from "@/components/ContactUs";
 import HomePage from "@/components/home"
 import Project from "@/components/Project";
 import Skills from "@/components/Skills";
-export default function mainpage() {
-    return (
+export default async function mainpage() {
+    await new Promise(resolve => setTimeout(resolve, 200)); 
+  return (
     <div>
     {/* this isa  home page bro  bro  */}
     <HomePage/>
